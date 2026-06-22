f0c8473d0d947aee8f56388b55adaa5a1cf3ff3a
elbahi03 <yassineelbahi44@gmail.com>
2026-06-23 00:58:38 +0100

    section hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import './App.css'
 import Header from "./componant/Header";
+import Hero from "./componant/hero";
 
 function App() {
 
   return (
     <>
        <Header/>
+       <Hero/>
     </>
   )
 }
---
+++ src/componant/Hero.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { useTranslation } from 'react-i18next';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import '../style/hero.css';
+import hero1 from '../asset/hero/hero1.jpg';
+import hero2 from '../asset/hero/hero2.jpg';
+import hero3 from '../asset/hero/hero3.jpg';
+import hero4 from '../asset/hero/hero4.jpg';
+import hero5 from '../asset/hero/hero5.jpg';
+
+
+import { Pagination, Autoplay } from 'swiper/modules';
+
+export default function Hero() {
+    const { t } = useTranslation();
+    return (
+        <div className="hero-wrapper">
+            <Swiper
+                pagination={{ dynamicBullets: true }}
+                autoplay={{ delay: 4000, disableOnInteraction: false }}
+                modules={[Pagination, Autoplay]}
+                className="mySwiper"
+            >
+                <SwiperSlide><img src={hero1} /></SwiperSlide>
+                <SwiperSlide><img src={hero2} /></SwiperSlide>
+                <SwiperSlide><img src={hero3} /></SwiperSlide>
+                <SwiperSlide><img src={hero4} /></SwiperSlide>
+                <SwiperSlide><img src={hero5} /></SwiperSlide>
+            </Swiper>
+
+            <div className="hero-content">
+                <h1 className="hero-site-name">
+                    {t("siteName")}
+                </h1>
+
+                <h3 className="hero-title">
+                    {t("heroTitle")}
+                </h3>
+
+                <button className="hero-btn">
+                    {t("volunteerNow")}
+                </button>
+            </div>
+        </div>
+    );
+}
